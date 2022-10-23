66386b8d284ab7a93ea5a6ee4a9334d7061bfd76
therayy <raafat@ibm.com>
2022-10-23 11:31:33 -0700

    console script post deployment
    Signed-off-by: therayy <raafat@ibm.com>

--- instances/cp4a-odm/post-deploy/console.sh
+++ instances/cp4a-odm/post-deploy/console.sh
@@ -1,12 +1,14 @@
-INGRESS_SUBDOMAIN=${INGRESS_SUBDOMAIN:-"oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.spec.defaultCertificate.name}'"}
-NAMESPACE=${NAMESPACE:-"tools"}
+INGRESS_DOMAIN=$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
+NAMESPACE=${NAMESPACE:-"odm"}
+echo ${NAMESPACE}
+echo ${INGRESS_DOMAIN}
 cat <<EOF |oc apply -f -
 apiVersion: console.openshift.io/v1
 kind: ConsoleLink
 metadata:
   name: cloud-pak-dashboard
 spec:
-  href: 'https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}'
+  href: 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'
   location: ApplicationMenu
   applicationMenu:
     section: IBM Cloud Paks
@@ -19,7 +21,7 @@ kind: ConsoleLink
 metadata:
   name: cloud-pak-access-info
 spec:
-  href: 'https://console-openshift-console.${INGRESS_SUBDOMAIN}/k8s/ns/tools/configmaps/icp4adeploy-cp4ba-access-info'
+  href: 'https://console-openshift-console.'$INGRESS_DOMAIN'/k8s/ns/tools/configmaps/icp4adeploy-cp4ba-access-info'
   location: ApplicationMenu
   applicationMenu:
     section: IBM Cloud Paks
@@ -32,7 +34,7 @@ kind: ConsoleLink
 metadata:
   name: decision-center
 spec:
-  href: 'https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/decisioncenter'
+  href: 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/decisioncenter'
   location: ApplicationMenu
   applicationMenu:
     section: Operational Decision Manager
@@ -45,7 +47,7 @@ kind: ConsoleLink
 metadata:
   name: decision-server-console
 spec:
-  href: 'https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/res'
+  href: 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/res'
   location: ApplicationMenu
   applicationMenu:
     section: Operational Decision Manager
@@ -58,7 +60,7 @@ kind: ConsoleLink
 metadata:
   name: decision-runner
 spec:
-  href: 'https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/DecisionRunner'
+  href: 'https://cpd-'$NAMESPACE'.'$INGRESS_SUBDOMAIN'/odm/DecisionRunner'
   location: ApplicationMenu
   applicationMenu:
     section: Operational Decision Manager
@@ -71,21 +73,22 @@ kind: ConsoleLink
 metadata:
   name: decision-server-runtime
 spec:
-  href: 'https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/DecisionService'
+  href: 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/DecisionService'
   location: ApplicationMenu
   applicationMenu:
     section: Operational Decision Manager
   text: Decision Server Runtime
 EOF
+
 echo Cloud Pak Dashboard
-echo https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}
+echo 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN''
 echo Cloud Pak access info
-echo https://console-openshift-console.${INGRESS_SUBDOMAIN}/k8s/ns/tools/configmaps/icp4adeploy-cp4ba-access-info
+echo 'https://console-openshift-console.'$INGRESS_DOMAIN'/k8s/ns/tools/configmaps/icp4adeploy-cp4ba-access-info'
 echo Decision Center
-echo https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/decisioncenter
+echo 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/decisioncenter'
 echo Decision Server Console
-echo https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/res
+echo 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/res'
 echo Decision Runner
-echo https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/DecisionRunner
+echo 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'/odm/DecisionRunner'
 echo Decision Server Runtime
-echo https://cpd-${NAMESPACE}.${INGRESS_SUBDOMAIN}/odm/DecisionService
+echo 'https://cpd-'$NAMESPACE'.'$INGRESS_DOMAIN'
