80466c9d71915b40abd17185138d73e9b3ca0c39
therayy <raafat@ibm.com>
2022-10-23 20:59:27 -0700

    clean up
    Signed-off-by: therayy <raafat@ibm.com>

--- instances/cp4a-db2-instance/db2-job/ldap-pod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-oc project tools
-ldap_pod=$(oc get pods -l role=ldap -o name)
-oc rsh ${ldap_pod} /bin/bash 
-/opt/ibm/ldap_scripts/addLdapUser.py -u icnadm -p Passw0rd -r user
-/opt/ibm/ldap_scripts/addLdapUser.py -u odmadm -p Passw0rd -r user
-
-echo done!
